ba1960e5a3ca3f277615b16076540d86f6879200
ahmed anwar <ahmed.anwar.ibrahem66@gmail.com>
2019-05-02 13:30:11 +0200

    final task for not senior

---
+++ LastTask/Controllers/ProfilesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using LastTask.Models;
+
+namespace LastTask.Controllers
+{
+    [Authorize(Roles = "AdminRole")]
+    public class ProfilesController : Controller
+    {
+        private LastTaskEntities db = new LastTaskEntities();
+
+        // GET: Profiles
+        public ActionResult Index()
+        {
+            var profiles = db.Profiles.Include(p => p.AspNetUser);
+            return View(profiles.ToList());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ LastTask/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LastTask.Startup))]
+namespace LastTask
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
