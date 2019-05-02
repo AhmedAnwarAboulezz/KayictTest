a6a8242b51933247e4b7ffab468f3c2114623bc0
ahmed anwar <ahmed.anwar.ibrahem66@gmail.com>
2019-05-02 15:37:50 +0200

    changes for senior app

--- LastTask/Controllers/ProfilesController.cs
+++ LastTask/Controllers/ProfilesController.cs
@@ -21,6 +21,25 @@ namespace LastTask.Controllers
             var profiles = db.Profiles.Include(p => p.AspNetUser);
             return View(profiles.ToList());
         }
+        public ActionResult ActivateUsers()
+        {
+            var usersnotactivate = db.Profiles.Where(c => c.AspNetUser.Status == 0 || c.AspNetUser.Status == null).ToList();
+            return View();
+        }
+        public ActionResult EnableActive(string userid)
+        {
+
+            if (userid != "")
+            {
+                var userdefault = db.AspNetUsers.Where(c => c.Id == userid).FirstOrDefault();
+                userdefault.Status = 1;
+                db.Entry(userdefault).State = EntityState.Modified;
+                db.SaveChanges();
+
+                return Json(new { message = "Activate done Successfully" });
+            }
+            return Json(new { message = "Something went wrong" });
+        }
 
         protected override void Dispose(bool disposing)
         {
